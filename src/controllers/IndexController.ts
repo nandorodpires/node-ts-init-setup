54d4aa4e496841d642fa874f6a1ebc4d7c2069a8
Fernando Rodrigues <nandorodpires@gmail.com>
2021-05-11 18:04:27 -0300

    delete: app.ts

--- src/controllers/IndexController.ts
+++ src/controllers/IndexController.ts
@@ -4,10 +4,6 @@ class IndexController {
     public async index(request: Request, response: Response): Promise<Response> {
         return response.json({ message: 'Route /' })
     }
-
-    public async users(request: Request, response: Response): Promise<Response> {
-        return response.json({ message: 'List of users' })
-    }
 }
 
 export default new IndexController()
