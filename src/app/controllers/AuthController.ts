8b944f4844b7c1a30f3c8123f1ec1364a547badb
Fernando Rodrigues <nandorodpires@gmail.com>
2021-05-14 08:56:12 -0300

    feat: Authentication and auth middleware

--- src/app/controllers/AuthController.ts
+++ src/app/controllers/AuthController.ts
@@ -1,7 +1,39 @@
 import { Request, Response } from 'express'
+import { getRepository } from 'typeorm'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+
+import User from '../models/User'
+
 class AuthController {
     async authenticate(request: Request, response: Response) {
-        return response.json({ message: 'Authenticate method' })
+        const repository = getRepository(User)
+        const { email, password } = request.body
+
+        try {
+            const user = await repository.findOne({ where: { email } })
+
+            if (!user) {
+                return response.status(401).json({})
+            }
+
+            const isValid = await bcrypt.compare(password, user.password)
+
+            if (!isValid) {
+                return response.status(401).json({ message: 'Email or password is not valid!' })
+            }
+
+            const token = jwt.sign({ id: user.id }, process.env.API_SECRET, { expiresIn: '1d' })
+
+            delete user.password
+
+            response.json({
+                user, token
+            })
+        } catch (error) {
+            const { code, message } = error
+            return response.status(400).json({ code, message })
+        }
     }
 }
 
--- src/app/middlewares/authMiddleware.ts
+++ src/app/middlewares/authMiddleware.ts
@@ -1,5 +1,28 @@
 import { Request, Response, NextFunction } from 'express'
+import jwt from 'jsonwebtoken'
+
+interface TokenPayload {
+    id: number,
+    iat: number,
+    exp: number
+}
 
 export default function authMiddleware(request: Request, response: Response, next: NextFunction) {
+    const { authorization } = request.headers
+
+    if (!authorization) {
+        return response.status(400).json({ message: '' })
+    }
+
+    const token = authorization.replace('Bearer', '').trim()
+
+    try {
+        const data = jwt.verify(token, process.env.API_SECRET)
+        const { id } = data as TokenPayload
+        request.userId = id
+    } catch (error) {
+
+    }
+
     return next()
 }
